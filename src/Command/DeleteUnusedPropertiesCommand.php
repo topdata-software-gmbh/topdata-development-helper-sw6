8a2fe2accd82e2186b27b50492ae34e0db9ada57
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2024-06-20 12:49:41 +0200

    topdata:development-helper:delete-unused-properties works probably.

--- src/Command/DeleteUnusedPropertiesCommand.php
+++ src/Command/DeleteUnusedPropertiesCommand.php
@@ -3,6 +3,9 @@
 namespace Topdata\TopdataDevelopmentHelperSW6\Command;
 
 use Doctrine\DBAL\Connection;
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,21 +16,25 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  * 06/2024 created
  */
 #[AsCommand(name: 'topdata:development-helper:delete-unused-properties')]
-class DeleteUnusedPropertiesCommand extends Command
+class DeleteUnusedPropertiesCommand extends AbstractCommand
 {
     protected static $defaultName = 'topdata:development-helper:delete-unused-properties';
 
     private Connection $connection;
+    private EntityRepository $propertyGroupRepository;
+    private EntityRepository $productRepository;
 
-    /**
-     * @param Connection $connection
-     */
-    public function __construct(Connection $connection)
-    {
+    public function __construct(
+        Connection $connection,
+        EntityRepository $propertyGroupRepository,
+        EntityRepository $productRepository
+    ) {
         parent::__construct();
         $this->connection = $connection;
+        $this->propertyGroupRepository = $propertyGroupRepository;
+        $this->productRepository = $productRepository;
     }
-
+    
 
     protected function configure()
     {
@@ -40,8 +47,66 @@ class DeleteUnusedPropertiesCommand extends Command
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        // TODO...
+        // ---- find them
+        $unusedProperties = $this->getUnusedProperties();
+        if (empty($unusedProperties)) {
+            $this->cliStyle->success('No unused properties found.');
+            return Command::SUCCESS;
+        }
+
+
+        // ---- delete them
+        $unusedPropertiesString = implode(',', array_map(fn($x) => '0x' . bin2hex($x) , $unusedProperties));
+        $sql = "DELETE FROM property_group_option WHERE id IN ($unusedPropertiesString)";
+        $this->connection->executeStatement($sql);
+
+
+        // ---- done
+        $this->cliStyle->success(sprintf('Deleted %d unused properties.', count($unusedProperties)));
 
         return Command::SUCCESS;
     }
+
+
+
+
+
+
+
+
+
+
+    private function getUnusedProperties(): array
+    {
+        $usedPropertyIds = $this->getUsedPropertyIds();
+        $this->cliStyle->green(count($usedPropertyIds) . ' used properties found.');
+
+        $allPropertyIds = $this->getAllPropertyIds();
+        $this->cliStyle->green(count($allPropertyIds) . ' all properties found.');
+
+        $ret = array_diff($allPropertyIds, $usedPropertyIds);
+        $this->cliStyle->green(count($ret) . ' unused properties found.');
+
+        return $ret;
+    }
+
+
+    private function getUsedPropertyIds(): array
+    {
+        $sql = 'SELECT DISTINCT property_group_option_id FROM product_property';
+
+        return $this->connection->executeQuery($sql)->fetchFirstColumn();
+    }
+
+
+
+    private function getAllPropertyIds(): array
+    {
+        $sql = 'SELECT id FROM property_group_option';
+
+        return $this->connection->executeQuery($sql)->fetchFirstColumn();
+    }
+    
+    
+    
 }
